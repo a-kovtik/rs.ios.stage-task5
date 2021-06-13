c02bbab20c6abd1c71036c438a3235fbd0fad9ee
a-kovtik <alekov.sol@gmail.com>
2021-06-14 00:00:27 +0200

    feat: add ex 1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,15 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var profit = 0
+                
+        for (index, currentPrice) in prices.enumerated() {
+            if let bestDayPrice = prices.dropFirst(index + 1).max() {
+                if currentPrice < bestDayPrice {
+                    profit += bestDayPrice - currentPrice
+                }
+            }
+        }
+        return profit
     }
 }
